a5f0be60281a1b5a5da1c85557242a3facf1c225
AlbertCorn <220711683@students.uajy.ac.id>
2024-12-04 15:13:16 +0700

    Update Tugas_Modul6_B_Albert_11683.py

--- Tugas_Modul6_B_Albert_11683.py
+++ Tugas_Modul6_B_Albert_11683.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 # Load the pre-trained model
 # Sesuaikan dengan path model Anda (model terbaik yang di dump dalam format .h5)
-model = load_model("D:\Semester 5\Pembelajaran mesin dan Pembelajaran Mendalam\Modul Convolutional Neural Network\Tugas6_B_11683\model_mobilenet.h5")
+model = load_model(r"model_mobilenet.h5")
 class_names = ["Matang", "Mentah"]
 
 # Function to preprocess and classify image
